0663e09109c40e524289ab85ffdf2068ef77e45b
Scott Blum <dragonsinth@gmail.com>
2018-12-16 13:35:08 -0500

    Integrate shard_iterator with suggestive (#10540)

---
+++ solrmonitor/mock.go
@@ -0,0 +1,16 @@
+package solrmonitor
+
+func NewMockSolrMonitor(state ClusterState, liveNodes []string) *SolrMonitor {
+	collections := map[string]*collection{}
+	for k, v := range state {
+		collections[k] = &collection{
+			cachedState: &parsedCollectionState{
+				collectionState: v,
+			},
+		}
+	}
+	return &SolrMonitor{
+		collections: collections,
+		liveNodes:   liveNodes,
+	}
+}
