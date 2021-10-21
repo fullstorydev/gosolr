f60588d1bf46a483af8d5a76f0827402bfd9b2b0
Hitesh <hitesh@fullstory.com>
2021-10-21 14:40:10 -0700

    formatting

--- solrmonitor/mock.go
+++ solrmonitor/mock.go
@@ -4,7 +4,7 @@ func NewMockSolrMonitor(state ClusterState, liveNodes []string) *SolrMonitor {
 	collections := map[string]*collection{}
 	for k, v := range state {
 		collections[k] = &collection{
-				collectionState: v,
+			collectionState: v,
 		}
 	}
 	return &SolrMonitor{
