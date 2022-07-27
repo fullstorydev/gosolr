a3a0f5f6d3a1bc461ebf7b0d36ddb1f31e716665
Hitesh <hitesh@fullstory.com>
2022-07-27 09:15:05 -0700

    adding type property to SolrCoreStatus

--- solrman/solrmanapi/cloudstatus.go
+++ solrman/solrmanapi/cloudstatus.go
@@ -36,4 +36,5 @@ type SolrCoreStatus struct {
 	HasStats     bool   // if false, core status could not be queried and following attributes are expected to be -1
 	NumDocs      int64  // total number of indexed documents
 	IndexSize    int64  // in bytes
+	Type         string // e.g. "NRT", "PULL", "TLOG"
 }
