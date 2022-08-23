ea842070cf8bdbdd426fc317ff1722ea99c2ea69
Justin Sweeney <justinsweeney@fullstory.com>
2022-08-23 10:26:01 -0400

    Adding all methods for ZkCli

--- solrmonitor/solrmonitor.go
+++ solrmonitor/solrmonitor.go
@@ -46,14 +46,21 @@ type SolrMonitor struct {
 	solrEventListener SolrEventListener      // to listen the solr cluster state
 }
 
-// Minimal interface solrmonitor needs (allows for mock ZK implementations).
+// Interface solrmonitor needs (allows for mock ZK implementations).
 type ZkCli interface {
+	Children(path string) ([]string, *zk.Stat, error)
 	ChildrenW(path string) ([]string, *zk.Stat, <-chan zk.Event, error)
+	Close()
+	Create(path string, data []byte, flags int32, acl []zk.ACL) (string, error)
+	Delete(path string, version int32) error
+	Exists(path string) (bool, *zk.Stat, error)
+	ExistsW(path string) (bool, *zk.Stat, <-chan zk.Event, error)
 	Get(path string) ([]byte, *zk.Stat, error)
 	GetW(path string) ([]byte, *zk.Stat, <-chan zk.Event, error)
-	ExistsW(path string) (bool, *zk.Stat, <-chan zk.Event, error)
+	SessionID() int64
+	Set(path string, contents []byte, version int32) (*zk.Stat, error)
 	State() zk.State
-	Close()
+	Sync(path string) (string, error)
 }
 
 // Create a new solrmonitor.  Solrmonitor takes ownership of the provided zkCli and zkWatcher-- they
