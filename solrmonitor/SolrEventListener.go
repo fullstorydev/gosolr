54dc95d46374ad27ddc234ffb510f28250194b82
patsonluk <patson@fullstory.com>
2023-06-15 11:50:30 -0700

    Reverted unnecessary change

--- solrmonitor/SolrEventListener.go
+++ solrmonitor/SolrEventListener.go
@@ -12,19 +12,23 @@ On startup, the events are fired in this order:
     3.2 SolrCollectionReplicaStatesChanged fires if Per replica state(PRS) is enable for collection.
     If PRS enable then it keeps watch on zk node /solr/collections/collname/state.json 's children. Those children represents state of each replica of collection
     (reference https://issues.apache.org/jira/browse/SOLR-15052, https://docs.google.com/document/d/1xdxpzUNmTZbk0vTMZqfen9R3ArdHokLITdiISBxCFUg/edit )
- 4. SolrClusterPropsChanged fires for the single clusterprops file
 
 After initialization it delivers events for following condition
+
  1. SolrLiveNodesChanged if solr live nodes changes
+
  2. SolrQueryNodesChanged if solr query node changes
+
  3. SolrCollectionsChanged if number of collections changes in solr cluster
+
  4. SolrCollectionStateChanged if collection's number of shards changes, or replica moves, or replica splits.
+
  5. SolrCollectionReplicaStatesChanged if collection's replica goes up/down, or becomes leader
- 6. SolrClusterPropsChanged if the cluster props are modified
 
  4. If collection get deleted - for non PRS
     4.1 SolrCollectionStateChanged fires for collection's state.json, which get updated for each shard/replica
     4.2	SolrCollectionStateChanged fires with all collections name except deleted collection.
+
  5. If collection get deleted - for PRS
     5.1 following events happen
     5.1.1 SolrCollectionStateChanged fires for collection's base state.json
@@ -46,7 +50,4 @@ type SolrEventListener interface {
 
 	// collection replica state changed
 	SolrCollectionReplicaStatesChanged(name string, replicaStates map[string]*PerReplicaState)
-
-	// cluster props changed
-	SolrClusterPropsChanged(clusterprops map[string]string)
 }
