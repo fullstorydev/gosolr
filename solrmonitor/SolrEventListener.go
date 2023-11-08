07b7549f153dd3b34da2b178c7335a1ab28d74b1
hitesh <hitesh@fullstory.com>
2023-11-08 14:13:36 -0800

    updated comments

--- solrmonitor/SolrEventListener.go
+++ solrmonitor/SolrEventListener.go
@@ -6,8 +6,8 @@ The client can register the SolrEventListener to listen to the solr cluster stat
 On startup, the events are fired in this order:
  1. SolrLiveNodesChanged fires for all the solr live nodes (node which keeps indexes). It keeps watch on zk node /solr/live_nodes 's children
  2. SolrCollectionsChanged fires for all collections that exist. It keeps watch on zk node /solr/collections 's children.
-    3.1 SolrCollectionStateChanged fires for each collection; keeps watch on zk node /solr/collections/collname/state.json
-    3.2 SolrCollectionReplicaStatesChanged fires if Per replica state(PRS) is enable for collection.
+    2.1 SolrCollectionStateChanged fires for each collection; keeps watch on zk node /solr/collections/collname/state.json
+    2.2 SolrCollectionReplicaStatesChanged fires if Per replica state(PRS) is enable for collection.
     If PRS enable then it keeps watch on zk node /solr/collections/collname/state.json 's children. Those children represents state of each replica of collection
     (reference https://issues.apache.org/jira/browse/SOLR-15052, https://docs.google.com/document/d/1xdxpzUNmTZbk0vTMZqfen9R3ArdHokLITdiISBxCFUg/edit )
  3. SolrClusterPropsChanged fires for the single clusterprops file
